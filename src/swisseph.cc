3b2c5c2d3ee16b3e9eac5cf34de81743ef5579ab
Bak Jeong Jun <dev_bjun@meshyarn.io>
2021-11-09 22:33:20 +0900

    #src: fix house function name

--- src/swisseph.cc
+++ src/swisseph.cc
@@ -50,13 +50,12 @@ Napi::Object init(Napi::Env env, Napi::Object exports) {
   /**
    * HOUSE
    */
-	exports.Set(Napi::String::New(env, "node_swe_date_conversion"), Napi::Function::New(env, node_swe_date_conversion));
-	exports.Set(Napi::String::New(env, "node_swe_julday"), Napi::Function::New(env, node_swe_julday));
-	exports.Set(Napi::String::New(env, "node_swe_revjul"), Napi::Function::New(env, node_swe_revjul));
-	exports.Set(Napi::String::New(env, "node_swe_utc_to_jd"), Napi::Function::New(env, node_swe_utc_to_jd));
-	exports.Set(Napi::String::New(env, "node_swe_jdet_to_utc"), Napi::Function::New(env, node_swe_jdet_to_utc));
-	exports.Set(Napi::String::New(env, "node_swe_jdut1_to_utc"), Napi::Function::New(env, node_swe_jdut1_to_utc));
-	exports.Set(Napi::String::New(env, "node_swe_utc_time_zone"), Napi::Function::New(env, node_swe_utc_time_zone));
+	exports.Set(Napi::String::New(env, "node_swe_houses"), Napi::Function::New(env, node_swe_houses));
+	exports.Set(Napi::String::New(env, "node_swe_houses_ex"), Napi::Function::New(env, node_swe_houses_ex));
+	exports.Set(Napi::String::New(env, "node_swe_houses_ex2"), Napi::Function::New(env, node_swe_houses_ex2));
+	exports.Set(Napi::String::New(env, "node_swe_houses_armc"), Napi::Function::New(env, node_swe_houses_armc));
+	exports.Set(Napi::String::New(env, "node_swe_houses_armc_ex2"), Napi::Function::New(env, node_swe_houses_armc_ex2));
+	exports.Set(Napi::String::New(env, "node_swe_houses_pos"), Napi::Function::New(env, node_swe_houses_pos));
 
 
   /**
