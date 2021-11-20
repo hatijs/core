d67e31ee08e6b9a3a4648471de94299ab5bf5b62
Jeong Jun Bak <dev_bjun@meshyarn.io>
2021-11-20 00:56:49 -0500

    #shell: fix node version

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --rm --volume $(pwd):/home/node/app --workdir /home/node/app -it node:bullseye /bin/bash
+docker run --rm --volume $(pwd):/home/node/app --workdir /home/node/app -it node:lts-bullseye /bin/bash
